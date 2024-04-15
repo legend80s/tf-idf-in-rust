f0ffeed006cb87a5ff9a73e8110093e995567ce5
legend80s <liuchzong@qq.com>
2024-04-15 11:48:46 +0800

    refactor: split into model.rs and lexer.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use anyhow::Result;
 use core::str;
 use std::{
-    collections::{BTreeMap, HashMap},
     env::args,
     fs::{self, File},
     io::{BufReader, BufWriter},
@@ -9,83 +8,11 @@ use std::{
     process::ExitCode,
     time::Instant,
 };
+use tf_idf_in_rust::{
+    calc_idf, calc_tf, read_term_freq_index_from_file, Lexer, TermFreq, TermFreqIndex,
+};
 use tiny_http::{Header, Method, Request, Response};
 use xml::{reader::XmlEvent, EventReader};
-
-// use BTreeMap for key sorted alphabetically
-type TermFreq = BTreeMap<String, usize>;
-type TermFreqIndex = HashMap<PathBuf, TermFreq>;
-
-struct Lexer<'a> {
-    content: &'a [char],
-}
-
-impl<'a> Lexer<'a> {
-    fn new(content: &'a [char]) -> Self {
-        Self { content }
-    }
-
-    fn chop(&mut self, n: usize) -> Option<&'a [char]> {
-        let token = Some(&self.content[0..n]);
-        self.content = &self.content[n..];
-
-        return token;
-    }
-
-    fn chop_while<P>(&mut self, mut predicate: P) -> Option<&'a [char]>
-    where
-        P: FnMut(&[char], usize) -> bool,
-    {
-        let mut n = 0;
-        while n < self.content.len() && predicate(self.content, n) {
-            n += 1;
-        }
-
-        return self.chop(n);
-    }
-
-    fn next_token(&mut self) -> Option<&'a [char]> {
-        if self.content.len() == 0 {
-            return None;
-        }
-
-        // 123
-        // 1.23
-        if self.content[0].is_numeric() {
-            return self.chop_while(|content, idx| {
-                let c = content[idx];
-                c.is_numeric() || c == '.' && content[idx + 1].is_numeric()
-            });
-        }
-
-        // hello
-        // gl4
-        // GL_INVALID_VALUE
-        if self.content[0].is_alphabetic() {
-            return self.chop_while(|content, idx| {
-                let c = content[idx];
-
-                c.is_alphanumeric() || c == '_'
-            });
-        }
-
-        // else then return a single character such as "√"
-        return self.chop(1);
-    }
-}
-
-impl<'a> Iterator for Lexer<'a> {
-    type Item = String;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let term = self
-            .next_token()
-            .map(|ch| ch.iter().collect())
-            .map(|ch: String| ch.to_ascii_uppercase().trim().to_string());
-
-        term
-    }
-}
 
 fn is_word(term: &str) -> bool {
     if term.is_empty() || term.chars().all(|c| c.is_ascii_punctuation()) {
@@ -93,12 +20,6 @@ fn is_word(term: &str) -> bool {
     }
 
     true
-}
-
-fn read_term_freq_index_from_file() -> Result<TermFreqIndex> {
-    let tf_index: TermFreqIndex = serde_json::from_reader(File::open("./assets/index.json")?)?;
-
-    Ok(tf_index)
 }
 
 fn search() -> Result<()> {
@@ -125,36 +46,6 @@ fn serve_404(request: Request) -> Result<()> {
     Ok(())
 }
 
-fn calc_tf(term: &str, doc: &TermFreq) -> f32 {
-    let count = *doc.get(term).unwrap_or(&0);
-    let total: usize = doc.values().sum();
-
-    count as f32 / total as f32
-}
-
-fn calc_idf(term: &str, tf_index: &TermFreqIndex) -> f32 {
-    let total_docs = tf_index.len();
-    let docs_contains_item = tf_index
-        .values()
-        .filter(|doc| doc.contains_key(term))
-        .count()
-        // fix negative f32
-        .max(1);
-
-    // println!("total_docs: {}", total_docs);
-    // println!("docs_contains_item: {}", docs_contains_item);
-
-    let n = total_docs;
-    let m = docs_contains_item;
-
-    // fix negative f32
-    // if docs_contains_item == 0 {
-    //     m = docs_contains_item + 1
-    // }
-
-    (n as f32 / m as f32).log10()
-}
-
 fn serve(mut request: Request) -> Result<()> {
     println!(
         "INFO: method: {:?}, url: {:?}",
@@ -170,8 +61,14 @@ fn serve(mut request: Request) -> Result<()> {
             println!("INFO: searching {body}");
 
             let mut result: Vec<(&PathBuf, f32)> = Vec::new();
+            let start = Instant::now();
             let tf_index = read_term_freq_index_from_file()?;
+            println!(
+                "read_term_freq_index_from_file costs: {:?}",
+                start.elapsed()
+            );
 
+            let start_calc = Instant::now();
             for (path, doc) in &tf_index {
                 let mut rank = 0f32;
                 for term in Lexer::new(&body.chars().collect::<Vec<_>>()) {
@@ -183,13 +80,15 @@ fn serve(mut request: Request) -> Result<()> {
 
                 result.push((path, rank));
             }
+            println!("calculate tf-idf costs: {:?}", start_calc.elapsed());
 
+            let start_sorting = Instant::now();
             result.sort_by(|(_, rank1), (_, rank2)| {
                 rank2
                     .partial_cmp(rank1)
                     .expect(&format!("{rank1} and {rank2} are not comparable"))
             });
-
+            println!("sorting costs: {:?}", start_sorting.elapsed());
             let mut vec = Vec::new();
 
             for (doc, rank) in result.iter().take(10) {
